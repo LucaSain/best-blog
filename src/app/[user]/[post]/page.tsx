d2b5386142a94910813a00925c63f3e95788000f
LucaSain <lucasainenco2@gmail.com>
2023-10-25 15:52:15 +0300

    Page: Post

---
+++ src/app/[user]/[post]/page.tsx
@@ -0,0 +1,16 @@
+export default function Post() {
+  return (
+    <>
+      <div className="pt-20 prose mx-4 sm:mx-auto min-h-screen h-max ">
+        <h1> PostName</h1>
+        <p>The post about the posts that have been made to be post</p>
+        <h2> Some subtitle</h2>
+        <p>The post about the posts that have been made to be post</p>
+        <p>The post about the posts that have been made to be post</p>
+      </div>
+      <div className="w-screen flex justify-center mb-4">
+        By <a className="ml-1 underline cursor-pointer">Mihnea Garbacica</a>
+      </div>
+    </>
+  );
+}
